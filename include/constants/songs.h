4cdf1008f12cfeabb132346e34024d3a9f20751c
Jared <jdcaperton@outlook.com>
2026-06-11 08:18:12 -0500

    Add mus_enpale_umbra (HSF): saw/square guitars, rs kit remap, kick thinning, ride to hat

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -488,7 +488,6 @@
 #define MUS_RG_TRAINER_TOWER        556 // MUS_RG_T_TOWER
 #define MUS_RG_SLOW_PALLET          557 // MUS_RG_SLOWMASARA
 #define MUS_RG_TEACHY_TV_MENU       558 // MUS_RG_TVNOIZE
-#define MUS_ENPALE_TEST             610
 
 // These PH_* constants are phoneme sounds used by the "bard" NPC (see src/bard_music.c and src/mauville_old_man.c).
 // Each comes in a triplet of PH_*_BLEND, PH_*_HELD, and PH_*_SOLO, and the name of each triplet incorporates the English phonetic sound it represents.
@@ -543,6 +542,8 @@
 #define PH_NURSE_BLEND              607
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
+#define MUS_ENPALE_INTRO            610
+#define MUS_ENPALE_UMBRA            611
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
