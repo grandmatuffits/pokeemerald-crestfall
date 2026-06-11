0a8c082cce3eb94a5e96e34a8ffff2c596b12971
Jared <jdcaperton@outlook.com>
2026-06-11 16:14:11 -0500

    Add Lamb of God Ruin rival battle theme; complete 9-song metal soundtrack

--- src/pokemon.c
+++ src/pokemon.c
@@ -5312,7 +5312,7 @@ u16 GetBattleBGM(void)
                 return MUS_VS_RIVAL;
             if (!StringCompare(GetTrainerNameFromId(TRAINER_BATTLE_PARAM.opponentA), gText_BattleWallyName))
                 return MUS_VS_TRAINER;
-            return MUS_VS_RIVAL;
+            return MUS_ENPALE_RIVAL;
         case TRAINER_CLASS_ELITE_FOUR:
             return MUS_VS_ELITE_FOUR;
         case TRAINER_CLASS_CHAMPION_FRLG:
