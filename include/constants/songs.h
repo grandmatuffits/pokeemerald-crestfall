21bc700d37e205110bb6aa4ab27c541089e326bf
Jared <jdcaperton@outlook.com>
2026-06-11 15:18:18 -0500

    Add Hero of the Day lab theme; PSG-lead-over-sampled-rhythm voicing

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -547,6 +547,7 @@
 #define MUS_ENPALE_MINSI            612
 #define MUS_ENPALE_WILD             613
 #define MUS_ENPALE_TRAINER          614
+#define MUS_ENPALE_LAB              615
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
