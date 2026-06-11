90e740e88d3053905ded710e184fe450b9c3d3bd
Jared <jdcaperton@outlook.com>
2026-06-11 14:52:09 -0500

    Add Lucretia trainer battle theme; loop distguitar sample (benefits all metal tracks)

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -546,6 +546,7 @@
 #define MUS_ENPALE_UMBRA            611
 #define MUS_ENPALE_MINSI            612
 #define MUS_ENPALE_WILD             613
+#define MUS_ENPALE_TRAINER          614
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
--- src/pokemon.c
+++ src/pokemon.c
@@ -5330,9 +5330,9 @@ u16 GetBattleBGM(void)
             return MUS_VS_FRONTIER_BRAIN;
         default:
             if (GetCurrentRegion() == REGION_KANTO)
-                return MUS_RG_VS_TRAINER;
+                return MUS_ENPALE_TRAINER;
             else
-                return MUS_VS_TRAINER;
+                return MUS_ENPALE_TRAINER;
         }
     }
     else
