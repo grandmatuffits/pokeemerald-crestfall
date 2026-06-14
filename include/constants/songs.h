9540b41c16738cb09be9a445e388d93312eafc8f
Jared <jdcaperton@outlook.com>
2026-06-14 10:00:30 -0500

    Add Ecstasy of Gold intro speech theme (route122 orchestral voicegroup); repoint Birch/Brutus speech music

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -557,6 +557,7 @@
 #define MUS_ENPALE_ROBIN            622
 #define MUS_ENPALE_COPPERGATE       623
 #define MUS_ENPALE_OMAR             624
+#define MUS_ENPALE_INTROSPEECH      625
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
