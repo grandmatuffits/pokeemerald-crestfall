efa2823cd546bf10d2f0b904abb279ab66a121a6
Jared <jdcaperton@outlook.com>
2026-06-16 12:40:17 -0500

    Play FireRed/LeafGreen intro before the Emerald title screen (force FRLG intro path in expansion_intro.c)

--- src/expansion_intro.c
+++ src/expansion_intro.c
@@ -276,15 +276,8 @@ void Task_HandleExpansionIntro(u8 taskId)
             ResetSpriteData();
             FreeAllSpritePalettes();
             DestroyTask(taskId);
-            if (IS_FRLG)
-            {
-                SetMainCallback2(CB2_SetUpIntroFrlg);
-            }
-            else
-            {
-                CreateTask(Task_Scene1_Load, 0);
-                SetMainCallback2(MainCB2_Intro);
-            }
+            // Force FireRed/LeafGreen intro (Emerald title screen stays via IS_FRLG=0 elsewhere)
+            SetMainCallback2(CB2_SetUpIntroFrlg);
         }
         break;
     }
