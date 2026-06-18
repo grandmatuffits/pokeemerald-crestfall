ffa8050c57d25d471ff73587d1ffc0d10a12657b
Jared <jdcaperton@outlook.com>
2026-06-17 21:29:19 -0500

    Add For Whom the Bell Tolls as title screen music (MUS_ENPALE_TITLE, song 626)

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -558,6 +558,7 @@
 #define MUS_ENPALE_COPPERGATE       623
 #define MUS_ENPALE_OMAR             624
 #define MUS_ENPALE_INTROSPEECH      625
+#define MUS_ENPALE_TITLE            626
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
